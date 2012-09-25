1c754c96730bd481922181e65849a3ca35631d23
jackokring <jackokring@gmail.com>
2012-09-25 01:27:21 +0100

    Use jar files of JogAMP

--- src/jogamp/newt/driver/DriverUpdatePosition.java
+++
@@ -1,9 +0,0 @@
-package jogamp.newt.driver;
-
-/** 
- * Interface tagging driver requirement of absolute positioning, ie. depend on parent position.
- */
-public interface DriverUpdatePosition {
-    /** Programmatic update the position */
-    void updatePosition();
-}
