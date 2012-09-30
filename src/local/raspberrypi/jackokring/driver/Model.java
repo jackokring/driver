8df4a587695c1255d0c1554602ee2d450f224f36
Simon Jackson (Netbook) <jackokring@gmail.com>
2012-09-30 08:28:51 +0100

    Better Model

--- src/local/raspberrypi/jackokring/driver/Model.java
+++ src/local/raspberrypi/jackokring/driver/Model.java
@@ -4,79 +4,100 @@ import local.raspberrypi.jackokring.util.*;
 
 public class Model extends Zero {
 	
-	float invMass = 1;
-	float x = 0;
-	float y = 0;
+	public float invMass = 1;
 	
-	Trig pointing = new Trig();//direction and wheel force
+	Trig pointing = new Trig();//direction
 	Trig sliding = new Trig();//velocity
 	Trig forces = new Trig();//body forces
 	Trig sheer = new Trig();//perp
 	Trig along = new Trig();//para
+	Trig location = new Trig();
 	
-	static Trig wind = new Trig();
-	static Trig calcTemp = new Trig();
+	public static Trig wind = new Trig();
+	public static Trig calcTemp = new Trig();
+	public static float rotary = (float) 1.0; 
 	
 	long updated = System.currentTimeMillis();
-	static long timeCache;
-	static float angleCache;
-	final static float halfp = (float) (Math.PI / 2);
+	public static long timeCache;
+	public final static float halfp = (float) (Math.PI / 2);
 	
-	float friction = 100;
-	float squirrel = 5;
-	float slip = (float) 0.5;
-	static float bounce = (float) 0.00001;
+	public float friction = 100;
+	public float squirrel = 5;
+	public float slip = (float) 0.5;
+	public static float bounce = (float) 0.00001;
 	
-	float drag = (float) 0.1;
+	public float drag = (float) 0.1;
+	public float torque = 0;//wheel torque
+	public float dir = 0;//steer
 	
-	public void update(float acc, float dir) {
+	public float revWheel = 0;//land speedometer would be proportional
+	public float invRadWheel = 1;
+	
+	public final float gear[] = { (float) 0.5, (float) 1 };
+	
+	public void update() {
 		timeCache = updated;
 		updated = System.currentTimeMillis();
 		if(updated == timeCache) return;
-		timeCache = updated - timeCache;//got millis
-		pointing.setRadius(acc * timeCache * invMass);//wheel drive
+		timeCache = updated - timeCache;//got milliseconds
+		pointing.setRadius(torque * timeCache * invMass);//wheel drive
 		dir += (Math.random() * 2 - 1) * bounce;//road jiggle
 		along.setAngle(pointing.angle);
 		sheer.setAngle((float) (pointing.angle + halfp));
 		addImpulses();
 		//do mass
 		forces.setRadius(forces.radius * invMass);
-		if(Math.abs(pointing.radius) > friction) {
-			pointing.setRadius(pointing.radius * slip);
-			inSkid();
-		}
-		forces.add(pointing);//wheel to body
 		//sheer
 		sheer.setDot(forces);
 		//acceleration
 		along.setDot(forces);
-		//steer
-		calcTemp.setAngle(pointing.angle);
-		calcTemp.setDot(sliding);
+		//limiting effect of rubber
+		//check wheel spin
+		calcTemp.setFrom(along);
+		calcTemp.setRadius(-calcTemp.radius);
+		calcTemp.add(pointing);
+		revWheel = invRadWheel;
+		if(Math.abs(calcTemp.radius) > friction) {
+			pointing.setRadius(pointing.radius * slip);
+			inSkid();
+			revWheel /= slip; 
+		}
 		if(Math.abs(sheer.radius) > friction) {
 			dir *= squirrel;
-			sheer.setRadius(sheer.radius * slip);
-			sliding.add(sheer);
+			sheer.setRadius(sheer.radius * (1 - slip));
 			inSkid();
 		} else {
-			sliding.setFrom(calcTemp);//no skid wheels
+			sheer.setRadius(0);//no skid wheels
 		}
-		calcTemp.twist(-halfp);
+		//calculation of steer and tilt
+		calcTemp.setAngle(pointing.angle);
+		calcTemp.setDot(sliding);
 		float r, v;
 		r = dir * (v = calcTemp.radius);//angular velocity
+		revWheel *= v;
+		calcTemp.twist(-halfp);
+		//steer
 		pointing.twist(timeCache * r);
 		r *= v;
 		calcTemp.setRadius(r);
 		forces.setFrom(calcTemp);//tilt
 		//move forward
+		sliding.add(pointing);
 		sliding.add(along);
+		sliding.add(sheer);
 		//position
-		x += sliding.x;
-		y += sliding.y;
+		location.add(sliding);
 	}
 	
 	void addCoriolis() {
-		
+		calcTemp.setFrom(location);
+		calcTemp.setRadius(calcTemp.radius * rotary * rotary);
+		forces.add(calcTemp);
+		calcTemp.setFrom(location);
+		calcTemp.setDot(sliding);
+		calcTemp.twist(halfp);
+		calcTemp.setRadius(calcTemp.radius * 2 * rotary);
+		forces.add(calcTemp);
 	}
 		
 	void addWind() {	
